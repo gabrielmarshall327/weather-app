06d785bfa6021f4769535fe24f675021ea4c7437
gabrielmarshall327 <gabrielmarshall327@gmail.com>
2022-12-20 16:04:26 -0600

    added the the default weather as the users location

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-import weatherApiKey from "./apiKeys.js";
+import apiKeysArr from "./apiKeys.js";
 
 let weather = {
-    //apiKey : process.env.apiKey, //api key
     fetchWeather: function (city) { //fetches the weather from api
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" //the api url with inputs of name and api key
             + city 
             + "&units=imperial&appid=" 
-            + weatherApiKey
+            + apiKeysArr[0]
         )
         .then((response) => response.json()) //pulls data from json?
         .then((data) => this.displayWeather(data)); //data is displayed by function
@@ -26,11 +25,64 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?mountains)" //can replace mountains with anything to change picture
     },
     search : function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
+};
+
+let geocode = {
+    reverseGeocode: function (latitude,longitude){
+        var api_key = apiKeysArr[1];
+      
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+      
+        var request_url = api_url
+          + '?'
+          + 'key=' + api_key
+          + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+          + '&pretty=1'
+          + '&no_annotations=1';
+      
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+      
+        request.onload = function() {
+          if (request.status === 200){
+            // Success!
+            var data = JSON.parse(request.responseText);
+            weather.fetchWeather(data.results[0].components.city);
+      
+          } else if (request.status <= 500){
+            // We reached our target server, but it returned an error
+      
+            console.log("unable to geocode! Response code: " + request.status);
+            var data = JSON.parse(request.responseText);
+            console.log('error msg: ' + data.status.message);
+          } else {
+            console.log("server error");
+          }
+        };
+      
+        request.onerror = function() {
+          // There was a connection error of some sort
+          console.log("unable to connect to server");
+        };
+      
+        request.send();  // make the request
+    },
+    getLocation: function () {
+        function success (data) {
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        }
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success,console.error);
+        }
+        else {
+            weather.fetchWeather("Denver");
+        }
+    }
 };
 
 document.querySelector(".search button").addEventListener("click" , function() {
@@ -43,4 +95,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
     }
 });
 
-weather.fetchWeather("Denver");
+geocode.getLocation();
